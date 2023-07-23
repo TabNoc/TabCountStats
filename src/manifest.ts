f24ee76b33bf452d87d3a48ab92b19ab14c3dd2f
TabNoc <meinich.spiel@gmail.com>
2023-07-23 16:33:35 +0200

    fix manifest V3 not working

--- src/manifest.ts
+++ src/manifest.ts
@@ -1,7 +1,7 @@
 import fs from 'fs-extra';
 import type { Manifest } from 'webextension-polyfill';
 import type PkgType from '../package.json';
-import { isDev, isFirefox, port, r } from '../scripts/utils';
+import { isDev, port, r } from '../scripts/utils';
 
 export async function getManifest() {
 	const pkg = await fs.readJSON(r('package.json')) as typeof PkgType;
@@ -9,7 +9,7 @@ export async function getManifest() {
 	// update this file to update this manifest.json
 	// can also be conditional based on your need
 	const manifest: Manifest.WebExtensionManifest = {
-		manifest_version: 3,
+		manifest_version: 2,
 		name: pkg.displayName || pkg.name,
 		version: pkg.version,
 		description: pkg.description,
@@ -19,7 +19,7 @@ export async function getManifest() {
 				id: true ? 'TabStats_dev@tabnoc.com' : 'TabStats@tabnoc.com',
 			},
 		},
-		action: {
+		browser_action: {
 			default_icon: './assets/icon-512.png',
 			default_title: 'TabAddonPage',
 			browser_style: true,
@@ -28,15 +28,12 @@ export async function getManifest() {
 		options_ui: {
 			page: './dist/options/index.html',
 			open_in_tab: true,
+			chrome_style: false,
 		},
-		background: isFirefox
-			? {
-				scripts: ['dist/background/index.mjs'],
-				type: 'module',
-			}
-			: {
-				service_worker: './dist/background/index.mjs',
-			},
+		background: {
+			page: './dist/background/index.html',
+			persistent: false,
+		},
 		icons: {
 			16: './assets/icon-512.png',
 			48: './assets/icon-512.png',
@@ -47,39 +44,27 @@ export async function getManifest() {
 			'sessions',
 			'storage',
 			'activeTab',
+			'http://*/',
+			'https://*/',
 		],
-		host_permissions: ['*://*/*'],
-		content_scripts: [
-			{
-				matches: [
-					'<all_urls>',
-				],
-				js: [
-					'dist/contentScripts/index.global.js',
-				],
-			},
-		],
+		content_scripts: [{
+			matches: ['http://*/*', 'https://*/*'],
+			js: ['./dist/contentScripts/index.global.js'],
+		}],
 		web_accessible_resources: [
-			{
-				resources: ['dist/contentScripts/style.css'],
-				matches: ['<all_urls>'],
-			},
+			'dist/contentScripts/style.css',
 		],
-		content_security_policy: {
-			extension_pages: isDev
-			// this is required on dev for Vite script to load
-				? `script-src \'self\' http://localhost:${port}; object-src \'self\'`
-				: 'script-src \'self\'; object-src \'self\'',
-		},
 	};
 
-	// FIXME: not work in MV3
-	if (isDev && false) {
+	if (isDev) {
 		// for content script, as browsers will cache them for each reload,
 		// we use a background script to always inject the latest version
 		// see src/background/contentScriptHMR.ts
 		delete manifest.content_scripts;
 		manifest.permissions?.push('webNavigation');
+
+		// this is required on dev for Vite script to load
+		manifest.content_security_policy = `script-src \'self\' http://localhost:${port}; object-src \'self\'`;
 	}
 
 	return manifest;
